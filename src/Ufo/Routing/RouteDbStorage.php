9635b186374ddceb3774c9785027acb849a9c571
enikeishik <enikeishik@gmail.com>
2019-01-16 15:05:59 +0300

    feat: db based routes storage

--- src/Ufo/Routing/RouteDbStorage.php
+++ src/Ufo/Routing/RouteDbStorage.php
@@ -15,22 +15,35 @@ use Ufo\Core\Section;
 class RouteDbStorage extends RouteStorage
 {
     /**
-     * @var Db
+     * @var \Ufo\Core\Db
      */
     protected $db;
     
+    /**
+     * @param \Ufo\Core\Db $db
+     */
     public function __construct(Db $db)
     {
         $this->db = $db;
+        
+        $sql = 'SELECT COUNT(*) AS cnt FROM #__sections';
+        if (empty($this->db->getValue($sql, 'cnt'))) {
+            throw new RouteStorageEmptyException();
+        }
     }
     
+    /**
+     * @param string $path
+     * @return ?\Ufo\Core\Section
+     */
     public function find(string $path): ?Section
     {
-        $sql = 'SELECT m.id, m.disabled, m.name FROM #__sections AS s' . 
-               ' INNER JOIN #__modules AS m ON s.module_id = m.id' . 
-               " WHERE s.path IN('" . implode("','", $path) . "')" . 
-               ' ORDER BY s.path DESC' . 
-               ' LIMIT 1';
+        $sql =  'SELECT s.title, m.vendor, m.name, m.disabled' . 
+                ' FROM #__sections AS s' . 
+                ' INNER JOIN #__modules AS m ON s.module = m.package' . 
+                " WHERE s.path IN('" . implode("','", $this->getPaths($path)) . "')" . 
+                ' ORDER BY s.path DESC' . 
+                ' LIMIT 1';
         $item = $this->db->getItem($sql);
         if (null === $item) {
             return null;
@@ -38,11 +51,16 @@ class RouteDbStorage extends RouteStorage
         return $this->getSection($path, $item);
     }
     
+    /**
+     * @param string $path
+     * @return ?\Ufo\Core\Section
+     */
     public function get(string $path): ?Section
     {
-        $sql = 'SELECT m.id, m.disabled, m.name FROM #__sections AS s' . 
-               ' INNER JOIN #__modules AS m ON s.module_id = m.id' . 
-               " WHERE s.path ='" . $path . "'";
+        $sql =  'SELECT s.title, m.vendor, m.name, m.disabled' . 
+                ' FROM #__sections AS s' . 
+                ' INNER JOIN #__modules AS m ON s.module = m.package' . 
+                " WHERE s.path ='" . $path . "'";
         $item = $this->db->getItem($sql);
         if (null === $item) {
             return null;
@@ -50,11 +68,25 @@ class RouteDbStorage extends RouteStorage
         return $this->getSection($path, $item);
     }
     
+    /**
+     * @param string $path
+     * @param array $item
+     * @return \Ufo\Core\Section
+     */
     protected function getSection(string $path, array $item): Section
     {
-        $item['dbless'] = false;
-        $item['callback'] = ''; //to prevent hack by SQL injection
+        $module = $this->getModule(array_merge(
+            $item, 
+            [
+                'callback' => '', //to prevent hack by SQL injection
+                'dbless' => false, 
+            ]
+        ));
         
-        return new Section(array_merge(['path' => $path], $item));
+        return new Section([
+            'path' => $path, 
+            'title' => $item['title'], 
+            'module' => $module, 
+        ]);
     }
 }
