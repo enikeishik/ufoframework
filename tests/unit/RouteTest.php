be00331abffb8512d5c0f348fe8bd4f7260b1457
enikeishik <enikeishik@gmail.com>
2019-01-16 10:40:12 +0300

    feat: more routes tests

--- tests/unit/RouteTest.php
+++ tests/unit/RouteTest.php
@@ -1,14 +1,17 @@
 <?php
-require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
-
 use \Ufo\Routing\Route;
 use \Ufo\Routing\RouteArrayStorage;
  
-class RouteTest extends \Codeception\Test\Unit
+class RouteTest extends BaseUnitTest
 {
     // tests
     public function testRouteParse()
     {
+        $this->expectedException(
+            \Ufo\Routing\RouteStorageEmptyException::class, 
+            function() { new RouteArrayStorage([]); }
+        );
+        
         $routeStorageData = require dirname(__DIR__) . '/_data/routes.php';
         $section = Route::parse('/section-with/callback', new RouteArrayStorage($routeStorageData));
         $this->assertNotNull($section);
