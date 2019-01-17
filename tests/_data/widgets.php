34ce61236912ba18a191c0f857859a4f6fc31ace
enikeishik <enikeishik@gmail.com>
2019-01-17 15:29:27 +0300

    feat: added vendor field into widgets

--- tests/_data/widgets.php
+++ tests/_data/widgets.php
@@ -2,27 +2,27 @@
 return [
     '/' => [
         'left col top' => [
-            ['module' => '', 'name' => 'gismeteo', 'title' => 'gismeteo widget', 'text' => '1 first wdg text'], 
-            ['module' => 'news', 'name' => '', 'title' => 'news widget', 'text' => '1 second wdg text'], 
+            ['vendor' => 'ufo', 'module' => '', 'name' => 'gismeteo', 'title' => 'gismeteo widget', 'text' => '1 first wdg text'], 
+            ['vendor' => 'ufo', 'module' => 'news', 'name' => '', 'title' => 'news widget', 'text' => '1 second wdg text'], 
         ], 
         'right col bottom' => [
-            ['module' => 'gallery', 'name' => '', 'title' => 'gallery widget', 'text' => '2 first wdg text'], 
+            ['vendor' => 'ufo', 'module' => 'gallery', 'name' => '', 'title' => 'gallery widget', 'text' => '2 first wdg text'], 
         ], 
     ], 
     '' => [
         'left col top' => [
-            ['module' => '', 'name' => 'cbrf', 'title' => 'currency widget', 'text' => 'USD GBR RUB EUR'], 
+            ['vendor' => 'ufo', 'module' => '', 'name' => 'cbrf', 'title' => 'currency widget', 'text' => 'USD GBR RUB EUR'], 
         ], 
     ], 
     '/document' => [
         'left col top' => [
-            ['module' => '', 'name' => 'images', 'title' => 'images widget', 'text' => 'images test'], 
+            ['vendor' => 'ufo', 'module' => '', 'name' => 'images', 'title' => 'images widget', 'text' => 'images test'], 
         ], 
         'right col bottom' => [
-            ['module' => 'gallery', 'name' => '', 'title' => 'gallery widget', 'text' => '2 first wdg text'], 
+            ['vendor' => 'ufo', 'module' => 'gallery', 'name' => '', 'title' => 'gallery widget', 'text' => '2 first wdg text'], 
         ], 
         'middle col bottom' => [
-            ['module' => 'articles', 'name' => '', 'title' => 'articles widget', 'text' => 'articles text'], 
+            ['vendor' => 'ufo', 'module' => 'articles', 'name' => '', 'title' => 'articles widget', 'text' => 'articles text'], 
         ], 
     ], 
 ];
