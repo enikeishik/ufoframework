438e6342c6738d80632cde0d02daf81a8dbcaadc
enikeishik <enikeishik@gmail.com>
2019-01-18 08:53:37 +0300

    feat: more widgets db based storage tests

--- tests/unit/WidgetsTest.php
+++ tests/unit/WidgetsTest.php
@@ -2,11 +2,17 @@
 use \Ufo\Core\Config;
 use \Ufo\Core\Db;
 use \Ufo\Core\Section;
+use \Ufo\Core\Widget;
 use \Ufo\Widgets\WidgetsArrayStorage;
 use \Ufo\Widgets\WidgetsDbStorage;
  
 class WidgetsTest extends BaseUnitTest
 {
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
     // tests
     public function testArrayStorage()
     {
@@ -63,6 +69,24 @@ class WidgetsTest extends BaseUnitTest
         $db = Db::getInstance($config);
         $storage = new WidgetsDbStorage($db);
         $this->assertEquals([], $storage->getWidgets(new Section()));
+        $this->assertEquals([], $storage->getWidgets(new Section(['path' => "'"]))); //to get null result
         $this->assertEquals([], $storage->getWidgets(new Section(['path' => '/'])));
+        
+        $this->tester->haveInDatabase(
+            'widgets', 
+            [
+                'id'            => 1001, 
+                'section_id'    => 0, 
+                'place'         => 'left col top', 
+                'widget'        => json_encode(new Widget([
+                    'vendor' => 'ufo', 
+                    'module' => '', 
+                    'name'   => 'gismeteo', 
+                    'title'  => 'gismeteo widget title', 
+                    'text'   => 'gismeteo widget content', 
+                ])), 
+            ]
+        );
+        $this->assertCount(1, $storage->getWidgets(new Section(['path' => '/'])));
     }
 }
