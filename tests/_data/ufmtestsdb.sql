01027ba4f736fe98a0125f5c98a3aa3449e6181d
enikeishik <enikeishik@gmail.com>
2019-02-28 15:51:57 +0300

    feat: storable cache tests and tests data

--- tests/_data/ufmtestsdb.sql
+++ tests/_data/ufmtestsdb.sql
@@ -14,6 +14,9 @@ CREATE TABLE IF NOT EXISTS `cache` (
   `key` varchar(255) NOT NULL default '',
   `value` text NOT NULL,
   `dtm` int(11) NOT NULL default '0',
+  `created` int(11) NOT NULL default '0',
+  `lifetime` int(11) NOT NULL default '0',
+  `savetime` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 TRUNCATE TABLE `cache`; /* cleanup from previous tests */
